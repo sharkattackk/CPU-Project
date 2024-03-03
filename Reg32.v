d024eba217b81f3e415f3844185f0f6830b83633
Cam Bennett <cambennett72@gmail.com>
2024-03-03 13:59:09 -0500

    reg

--- Reg32.v
+++ Reg32.v
@@ -1,11 +1,9 @@
 module Reg32(input clr, clk, Rin, input[31:0] d, output reg[31:0] q);
-    always @(posedge clk or posedge clr) 
+    always @*
     begin
 		if(clr)
 			q <= 32'h00000000;
 		else if(Rin)
 			q <= d;
-		else
-			q<= 32'h00000001;
 	end
 endmodule
