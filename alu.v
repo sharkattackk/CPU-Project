f0d210ffa1024858ad8bacf4f0e6d3b2971e761d
Cam Bennett <cambennett72@gmail.com>
2024-03-03 17:50:19 -0500

    or working

--- alu.v
+++ alu.v
@@ -1,15 +1,16 @@
 module alu(
   input wire 	[31:0] Rb,
   input wire	[31:0] Ry,
-  input AND,
+  input AND, OR,
   output reg [31:0] resultLo
   );
 
   //declare each opcode as the operation name for readability
 
   //declare the output wires for each operation
-  wire	[31:0] and_result;
+  wire	[31:0] and_result, or_result;
   alu_and and_module(Rb, Ry, and_result);
+  alu_or or_module(Rb, Ry, or_result);
 
 
 
@@ -18,6 +19,10 @@ module alu(
         begin
           resultLo = and_result;
         end
+	 else if(OR)
+			begin
+				resultLo = or_result;
+			end
     end
 
 
--- datapath.v
+++ datapath.v
@@ -1,7 +1,7 @@
 module datapath( input R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in,
 				 input R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out,
 				 input wire PCin, PCout, IRin, Yin, Zin, MARin, MDRin, MDRout, HIin, HIout, LOin, LOout, IncPC, Zhighout, Zlowout, Read, 
-				 input AND, InPortout, Cout, 
+				 input AND, OR, InPortout, Cout, 
 				 input [31:0] MDatain,
 				 input wire clk, clr
                );
@@ -94,6 +94,7 @@ module datapath( input R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9i
                     .Rb(BusMuxOut),
                     .Ry(BusMuxIn_Y),
                     .AND(AND),
+						  .OR(OR),
                     .resultLo(C_data_out)
                 );
 
